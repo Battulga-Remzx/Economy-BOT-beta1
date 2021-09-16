073a32ac3dc66b243863aa14169577ca8d212bce
Glitch (remzx-official-economy-bot-active) <none>
2021-09-16 09:26:34 +0000

    ⛰🍚 Checkpoint ./botConfig.js:36148879/1

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "ODg3Mzk4MDExOTAxMTk0MjQw.YUDjzQ.LSsFB8rMJYrm1Eophoz7IpYS7k",
+    token: "ODg3Mzk4MDExOTAxMTk0MjQw.YUDjzQ.LSsFB8rMJYrm1Eophoz7IpYS7kE",
     prefix: "~",
     admins: [
         "715428540954181714"
