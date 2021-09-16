620dd8841ced770db0574177c8252f8143618010
Glitch (remzx-official-economy-bot-active) <none>
2021-09-16 07:45:33 +0000

    🎰🎱 Checkpoint ./botConfig.js:36148879/3

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "ODg3Mzk4MDExOTAxMTk0MjQw.YUDjzQ.LSsFB8rMJYrm1Eophoz7IpYS7kE",
+    token: "ODg3Mzk4MDExOTAxMTk0MjQw.YUDjzQ.LSsFB8rMJYrm1Eophoz7IpYS7k",
     prefix: "~",
     admins: [
         "715428540954181714"
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
-  client.user.setActivity("Coded by Remzx O");
+  client.user.setActivity("Coded by Remzx Official");
 };
