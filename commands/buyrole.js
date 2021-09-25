5875f44656da6bfed277558df44010a306201760
Glitch (remzx-official-economy-bot-active) <none>
2021-09-25 07:11:51 +0000

    ⛄️🚉 Checkpoint ./commands/buyrole.js:36148879/2540

--- commands/buyrole.js
+++ commands/buyrole.js
@@ -4,25 +4,25 @@ exports.execute = async (client, message, args) => {
   let userBalance = client.eco.fetchMoney(message.author.id);
   if (userBalance.amount < 1) return message.channel.send("Чи яг л ядуу хүн шиг харагдаж байна.");
   let role = args[0];
-  if (!role) return message.channel.send("Ямар Role авахаа хойно нь бичнэ үү");
-  let hasRole = client.buyrole[role.toLowerCase()];
-  if (!hasRole || hasRole == undefined) return message.reply("Ийм Role байхгүй байна хүсвэл Role store ын захирал руу хүсэлт гаргаж болно шүү ");
-  let isRoleBalanceEnough = (userBalance.amount >= hasRole.cost);
-  if (!isRoleBalanceEnough) return message.reply("Таний дансан дахь үлдэгдэл хүрэлцэхгүй байна "+hasRole.cost+" төгрөгөөр энэ Role ыг авна.");
-  let buyrole = client.eco.removeMoney(message.author.id, hasRole.cost);
+  if (!role) return message.channel.send("Чи юу худалдаж авах вэ?");
+  let hasRole = client.shop[role.toLowerCase()];
+  if (!hasRole || hasRole == undefined) return message.reply("Ийм бараа байхгүй байна");
+  let isBalanceEnough = (userBalance.amount >= hasRole.cost);
+  if (!isBalanceEnough) return message.reply("Таний дансан дахь үлдэгдэл хүрэлцэхгүй байна "+hasRole.cost+" төгрөгөөр энэ барааг авч болно.");
+  let buy = client.eco.removeMoney(message.author.id, hasRole.cost);
   
-  let roleStruct = {
+  let itemStruct = {
     name: role.toLowerCase(),
     prize: hasRole.cost
   }
   
   
-  client.db.push(`roles_${message.authot.id}`, roleStruct);
-  return message.channel.send(`**${role}** амжилттай авагдлаа **:dollar: ${hasRole.cost}** Rolestore ын захирал орж ирээд Role ыг тань өгөх болно`);
+  client.db.push(`roles_${message.author.id}`, itemStruct);
+  return message.channel.send(`Худалдан авалт **${role}** амжилттай **:dollar: ${hasRole.cost}**  төгрөг болло.`);
 }
 
 exports.help = {
   name: "buyrole",
-  aliases: [],
+  aliases: [], 
   usage: `buyrole <role>`
 };
