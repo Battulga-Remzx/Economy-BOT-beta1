2f0e418c7c3dff2f6b1479562553d851320f3a89
Glitch (remzx-official-economy-bot-active) <none>
2021-10-10 15:43:33 +0000

    👄🐞 Checkpoint ./commands/rob.js:36148879/2534

--- commands/rob.js
+++ commands/rob.js
@@ -1,32 +1,38 @@
+const { MessageEmbed } = require("discord.js")
+
 exports.execute = async (client, message, args) => {
-  let member = message.mentions.members.first()
-  let targetuser = await client.db.fetch(`money_${member.id}`) // fetch mentioned users balance
-  let author = await client.db.fetch(`money_${message.author.id}`) // fetch authors balance
+
+  let user = message.mentions.members.first()
+    let targetuser = await client.db.fetch(`money_${user.id}`) // fetch mentioned users balance
+    let author = await client.db.fetch(`money_${message.author.id}`) // fetch authors balance
 
 
-    if (!member) {
-        return message.channel.send('Sorry, you forgot to mention somebody.')
+    if (!user) {
+        return message.channel.send('дээрэмдэх хүнээ сонгоно уу!')
     }
-    if (author < 250) { // if the authors balance is less than 250, return this.
+    if (author < 50000) { // if the authors balance is less than 250, return this.
         return message.channel.send(':x: You need atleast 250$ to rob somebody.')
     }
 
     if (targetuser < 0) { // if mentioned user has 0 or less, it will return this.
-        return message.channel.send(`:x: ${member.user.username} does not have anything to rob.`)
+        return message.channel.send(`:x: ${user.user.username}-д хэтэрхий бага мөнгөтэй болсон байна`)
     }
-  let random = Math.floor(Math.random() * 200) + 1; // random number 200-1, you can change 200 to whatever you'd like
 
 
-    let embed = new Discord.RichEmbed()
+    let random = Math.floor(Math.random() * 5000) + 1; // random number 200-1, you can change 200 to whatever you'd like
+
+
+    let embed = new MessageEmbed()
     .setDescription(`${message.author} you robbed ${user} and got away with ${random}!`)
     .setColor("GREEN")
     .setTimestamp()
     message.channel.send(embed)
 
 
-    db.subtract(`money_${user.id}`, random)
-    db.add(`money_${message.author.id}`, random)
+    client.db.subtract(`money_${user.id}`, random)
+    client.db.add(`money_${message.author.id}`, random)
 }
+
 exports.help = {
   name: "rob",
   aliases: [],
