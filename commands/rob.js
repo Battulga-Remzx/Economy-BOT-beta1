ef9ffeaa24b5ef139c95b0034a30e3bb0af8f7ec
Glitch (remzx-official-economy-bot-active) <none>
2021-10-10 15:19:43 +0000

    👾☕️ Checkpoint ./commands/rob.js:36148879/887 ./tall:36148879/2

--- commands/rob.js
+++ commands/rob.js
@@ -1,39 +1,19 @@
-const { MessageEmbed } = require("discord.js");
+const { db } = require("quick.eco");
 
 exports.execute = async (client, message, args) => {
   let member = message.mentions.members.first();
-  let targetuser = client.eco.fetch(`money_${member.id}`); // fetch mentioned users balance
-  let author = client.eco.fetch(`money_${message.author.id}`); // fetch authors balance
+  let targetmember = db.fetch(`money_${member.id}`); // fetch mentioned users balance
+  let author = db.fetch(`money_${message.author.id}`); // fetch authors balance
 
   if (!member) {
     return message.channel.send("Sorry, you forgot to mention somebody.");
   }
-  if (author < 250) {
-    // if the authors balance is less than 250, return this.
-    return message.channel.send(":x: You need atleast 250$ to rob somebody.");
+  if (author < 250) return message.channel.send(":x: You need atleast 250$ to rob somebody.");
   }
 
-  if (targetuser < 0) {
-    // if mentioned user has 0 or less, it will return this.
-    return message.channel.send(
-      `:x: ${member.user.username} does not have anything to rob.`
-    );
-  }
+  
 
-  let random = Math.floor(Math.random() * 200) + 1; // random number 200-1, you can change 200 to whatever you'd like
-
-  let embed = new MessageEmbed()
-    .setDescription(
-      `${message.author} you robbed ${member} and got away with ${random}!`
-    )
-    .setColor("GREEN")
-    .setTimestamp();
-  message.channel.send(embed);
-
-  client.eco.subtract(`money_${member.id}`, random);
-  client.eco.add(`money_${message.author.id}`, random);
-};
-
+  
 exports.help = {
   name: "rob",
   aliases: [],
