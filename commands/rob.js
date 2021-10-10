6a837a5027a7dde87384c7dcd452ca9e6c8fa61d
Glitch (remzx-official-economy-bot-active) <none>
2021-10-10 14:44:31 +0000

    🙈🐇 Checkpoint ./commands/rob.js:36148879/2776

--- commands/rob.js
+++ commands/rob.js
@@ -4,25 +4,16 @@ exports.execute = async (client, message, args) => {
   let user = message.mentions.members.first();
   let targetuser = await client.eco.fetch(`money_${user.id}`); // fetch mentioned users balance
   let author = await client.eco.fetch(`money_${message.author.id}`); // fetch authors balance
-
-  if (!user) {
-    return message.channel.send("Sorry, you forgot to mention somebody.");
-  }
-  if (author < 250) {
-    // if the authors balance is less than 250, return this.
-    return message.channel.send(":x: You need atleast 250$ to rob somebody.");
-  }
-
-  if (targetuser < 0) {
-    // if mentioned user has 0 or less, it will return this.
-    return message.channel.send(
+  if (!user) return message.channel.send("Sorry, you forgot to mention somebody.");
+  if (author < 250) return message.channel.send(":x: You need atleast 250$ to rob somebody.");
+  if (targetuser < 0) return message.channel.send(
       `:x: ${user.user.username} does not have anything to rob.`
     );
   }
 
   let random = Math.floor(Math.random() * 200) + 1; // random number 200-1, you can change 200 to whatever you'd like
 
-  let embed = new MessageEmbed()
+let embed = new MessageEmbed() 
     .setDescription(
       `${message.author} you robbed ${user} and got away with ${random}!`
     )
@@ -32,5 +23,10 @@ exports.execute = async (client, message, args) => {
 
   client.eco.subtract(`money_${user.id}`, random);
   client.eco.add(`money_${message.author.id}`, random);
-};
+}
 
+exports.help = {
+  name: "rob",
+  aliases: [],
+  usage: `rob <member>`
+};
