8373637d6e06433656fe70710e1cbd6e3c60539e
Glitch (remzx-official-economy-bot-active) <none>
2021-10-10 16:06:51 +0000

    🔦🏘 Checkpoint ./commands/rob.js:36148879/2808

--- commands/rob.js
+++ commands/rob.js
@@ -1,37 +1,40 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
+  let user = message.mentions.members.first();
+  let targetuser = await client.db.fetch(`money_${user.id}`); // fetch mentioned users balance
+  let author = await client.db.fetch(`money_${message.author.id}`); // fetch authors balance
 
-  let user = message.mentions.members.first()
-    let targetuser = await client.db.fetch(`money_${user.id}`) // fetch mentioned users balance
-    let author = await client.db.fetch(`money_${message.author.id}`) // fetch authors balance
+  if (!user) {
+    return message.channel.send(":x: дээрэмдэх хүнээ сонгоно уу!");
+  }
+  if (author < 50000) {
+    // if the authors balance is less than 250, return this.
+    return message.channel.send(":x: You need atleast 250$ to rob somebody.");
+  }
 
+  if (targetuser < 5000) {
+    // if mentioned user has 0 or less, it will return this.
+    return message.channel.send(
+      `:x: ${user.user.username} хэтэрхий бага мөнгөтэй болсон байна`
+    );
+  }
 
-    if (!user) {
-        return message.channel.send(':x: дээрэмдэх хүнээ сонгоно уу!')
-    }
-    if (author < 50000) { // if the authors balance is less than 250, return this.
-        return message.channel.send(':x: You need atleast 250$ to rob somebody.')
-    }
+  let random = Math.floor(Math.random() * 5000) + 1; // random number 200-1, you can change 200 to whatever you'd like
 
-    if (targetuser < 5000) { // if mentioned user has 0 or less, it will return this.
-        return message.channel.send(`:x: ${user.user.username} хэтэрхий бага мөнгөтэй болсон байна`)
-    }
-
-
-    let random = Math.floor(Math.random() * 5000) + 1; // random number 200-1, you can change 200 to whatever you'd like
-
-
-    let embed = new MessageEmbed()
-    .setDescription(`${user} хөөе  ${message.author} чамайг ${random} төгрөг-өөр дээрэмдлээ !`)
+  if (random.onCooldown) return message.reply(`${random.time.minutes} цаг ${random.time.seconds}`)
+  
+  let embed = new MessageEmbed()
+    .setDescription(
+      `${user} хөөе  ${message.author} чамайг ${random} төгрөг-өөр дээрэмдлээ !`
+    )
     .setColor("GREEN")
-    .setTimestamp()
-    message.channel.send(embed)
+    .setTimestamp();
+  message.channel.send(embed);
 
-
-    client.db.subtract(`money_${user.id}`, random)
-    client.db.add(`money_${message.author.id}`, random)
-}
+  client.db.subtract(`money_${user.id}`, random);
+  client.db.add(`money_${message.author.id}`, random);
+};
 
 exports.help = {
   name: "rob",
