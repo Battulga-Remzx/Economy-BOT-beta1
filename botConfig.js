82424579b886e059514e596c5e3a07950397c62d
Glitch (remzx-official-economy-bot-active) <none>
2021-10-10 19:19:01 +0000

    🙅🚥 Checkpoint ./commands/rob.js:36148879/11 ./botConfig.js:36148879/120

--- botConfig.js
+++ botConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   token: "ODg3Mzk4MDExOTAxMTk0MjQw.YUDjzQ.ROF8hs4iRt-M9d4MDym29EpZ9qg",
   prefix: "~",
   admins: ["715428540954181714"],
-  mafias: ["715428540954181714","877484899517468712","765593479010320424"],
+  mafias: ["765593479010320424","877484899517468712","811447438472380459"],
   debug: true,
 
   countChannel: "countChannelID"
