181302e3b9aa97bde4c380877d8c75fe947d85b1
Glitch (remzx-official-economy-bot-active) <none>
2021-10-10 18:24:40 +0000

    🏁📯 Checkpoint ./index.js:36148879/3367

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({ disableMentions: 'everyone' });
+const client = new Discord.Client({ disableMentions: "everyone" });
 const Eco = require("quick.eco");
 client.eco = new Eco.Manager(); // quick.eco
 client.db = Eco.db; // quick.db
 client.config = require("./botConfig");
+
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 //Дэлгүүр
@@ -24,7 +25,7 @@ client.shop = {
     cost: 800000
   },
   pc: {
-    cost:600000
+    cost: 600000
   },
   iphonex: {
     cost: 1500000
@@ -33,46 +34,44 @@ client.shop = {
     cost: 3000000
   },
   ferrari: {
-    cost:20000000
-  },
+    cost: 20000000
+  }
 };
 client.home = {
-mongol_ger: {
-  cost: 1000000
-},
-  house: {
-cost: 7000000
+  mongol_ger: {
+    cost: 1000000
   },
+  house: {
+    cost: 7000000
+  }
 };
 
-  const fs = require("fs");
+const fs = require("fs");
 
 fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(f => {
-        if (!f.endsWith(".js")) return;
-        const event = require(`./events/${f}`);
-        let eventName = f.split(".")[0];
-        client.on(eventName, event.bind(null, client));
-    });
+  if (err) return console.error(err);
+  files.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    const event = require(`./events/${f}`);
+    let eventName = f.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
 });
 
 fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(f => {
-        if (!f.endsWith(".js")) return;
-        let command = require(`./commands/${f}`);
-        client.commands.set(command.help.name, command);
-        command.help.aliases.forEach(alias => {
-            client.aliases.set(alias, command.help.name);
-        });
+  if (err) return console.error(err);
+  files.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    let command = require(`./commands/${f}`);
+    client.commands.set(command.help.name, command);
+    command.help.aliases.forEach(alias => {
+      client.aliases.set(alias, command.help.name);
     });
+  });
 });
 exports.keepAlive = (client, Message, Args) => {
-const keepAlive = require("./server.js");
-keepAlive();
-}
-
-
+  const keepAlive = require("./server.js");
+  keepAlive();
+};
 
 client.login(client.config.token);
