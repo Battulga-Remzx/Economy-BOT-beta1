645bf376fe89c2d3a74e2fd7b3f51fcc90842a1a
Glitch (remzx-official-economy-bot-active) <none>
2021-10-11 00:20:04 +0000

    👾🎊 Checkpoint ./commands/slots.js:36148879/8715

--- commands/slots.js
+++ commands/slots.js
@@ -1,58 +1,77 @@
-const slotItems = [":Grape:", ":Watermelon:", "🍊", ":Apple:", ":slot_machine:", ":Strawberry:", ":cherries:"];
-const Discord = require('discord.js');
+const slotItems = [
+  ":Grape:",
+  ":Watermelon:",
+  "🍊",
+  ":Apple:",
+  ":slot_machine:",
+  ":Strawberry:",
+  ":cherries:"
+];
+const Discord = require("discord.js");
 
-module.exports = {
-	help: {
-		name: "slots",
-		description: "Play slots!",
-		aliases: ["sl"],
-		category: "Economy"
-	},
+exports.execute = async (client, message, args) => {
+  let moneydb = client.eco.fetchMoney(message.author.id);
+  let user = message.author;
+  let money = parseInt(args[0]);
 
-run: async (client, message, args) => {
+  let win = false;
 
-    let user =  let authordata = client.eco.fetchMoney(message.author.id) 
-
-    let moneydb = await client.db.fetch(`money_${message.guild.id}_${user.id}.pocket`)
-
-    let money = parseInt(args[0]);
-
-    let win = false;
-
-    let moneymore = new Discord.MessageEmbed()
+  let moneymore = new Discord.MessageEmbed()
     .setColor("#FFFFFF")
-    .setDescription(`<a:false:737764891657633814> You are betting more than you have`);
+    .setDescription(
+      `<a:false:737764891657633814> You are betting more than you have`
+    );
 
-    let moneyhelp = new Discord.MessageEmbed()
+  let moneyhelp = new Discord.MessageEmbed()
     .setColor("#FFFFFF")
     .setDescription(`<a:false:737764891657633814> Specify an amount`);
 
-    if (!money) return message.channel.send(moneyhelp);
-    if (money > moneydb) return message.channel.send(moneymore);
+  if (!money) return message.channel.send(moneyhelp);
+  if (money > moneydb) return message.channel.send(moneymore);
 
-    let number = []
-    for (let i = 0; i < 3; i++) { number[i] = Math.floor(Math.random() * slotItems.length); }
+  let number = [];
+  for (let i = 0; i < 3; i++) {
+    number[i] = Math.floor(Math.random() * slotItems.length);
+  }
 
-    if (number[0] == number[1] && number[1] == number[2]) { 
-        money *= 9
-        win = true;
-    } else if (number[0] == number[1] || number[0] == number[2] || number[1] == number[2]) { 
-        money *= 2
-        win = true;
-    }
-    if (win) {
-        let slotsEmbed1 = new Discord.MessageEmbed()
-            .setDescription(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\n\nYou won ${money} coins`)
-            .setColor("#FFFFFF")
-        message.channel.send(slotsEmbed1)
-        await client.db.add(`money_${message.guild.id}_${user.id}.pocket`, money)
-    } else {
-        let slotsEmbed = new Discord.MessageEmbed()
-            .setDescription(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\n\nYou lost ${money} coins`)
-            .setColor("#FFFFFF")
-        message.channel.send(slotsEmbed)
-        await client.db.subtract(`money_${message.guild.id}_${user.id}.pocket`, money)
-		}
-
-	}
-}
+  if (number[0] == number[1] && number[1] == number[2]) {
+    money *= 9;
+    win = true;
+  } else if (
+    number[0] == number[1] ||
+    number[0] == number[2] ||
+    number[1] == number[2]
+  ) {
+    money *= 2;
+    win = true;
+  }
+  if (win) {
+    let slotsEmbed1 = new Discord.MessageEmbed()
+      .setDescription(
+        `${slotItems[number[0]]} | ${slotItems[number[1]]} | ${
+          slotItems[number[2]]
+        }\n\nYou won ${money} coins`
+      )
+      .setColor("#FFFFFF");
+    message.channel.send(slotsEmbed1);
+    await client.db.add(`money_${message.guild.id}_${user.id}.pocket`, money);
+  } else {
+    let slotsEmbed = new Discord.MessageEmbed()
+      .setDescription(
+        `${slotItems[number[0]]} | ${slotItems[number[1]]} | ${
+          slotItems[number[2]]
+        }\n\nYou lost ${money} coins`
+      )
+      .setColor("#FFFFFF");
+    message.channel.send(slotsEmbed);
+    await client.db.subtract(
+      `money_${message.guild.id}_${user.id}.pocket`,
+      money
+    );
+  }
+};
+exports.help = {
+  name: "---------------Мөрийтэй тоглоом----",
+  aliases: ["slots"],
+  usage: `slot <money>`
+};
