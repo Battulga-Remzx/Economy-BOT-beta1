7815426490beeb9609364a129e9157e19455f062
Glitch (remzx-official-economy-bot-active) <none>
2021-10-13 20:37:50 +0000

    🛍😄 Checkpoint ./commands/sell.js:36148879/548

--- commands/sell.js
+++ commands/sell.js
@@ -1,8 +1,16 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
-  
-  let sellItem = client.shop[x.toLowerCase()];
-  if (!sellItem || sellItem == undefined)
+  let author = message.author
+  let x = client.db.get(`items_${message.author.id}`);
+  let sell = client.db.subtract(`items_${message.author.id}`, 1);
+  if (!sell || sell == undefined)
     return message.reply("ийм бараа байхгүй байна");
+let buy = client.eco.removeMoney(message.author.id, sell);
+};
+
+exports.help = {
+  name: "---------------Item ашиглах----------",
+  aliases: ["use"], 
+  usage: `use <x>`
 };
