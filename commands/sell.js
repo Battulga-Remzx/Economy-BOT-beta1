b9fa520f88cb2ffe755be93b5f5d0a8c4f6f52fb
Glitch (remzx-official-economy-bot-active) <none>
2021-10-13 02:39:18 +0000

    🐜🏛 Checkpoint ./commands/sell.js:36148879/816

--- commands/sell.js
+++ commands/sell.js
@@ -1,1 +1,19 @@
-const c = 100
+const { MessageEmbed } = require("discord.js")
+
+exports.execute = async (client, message , args) => {
+
+  let user = message.author;
+
+        if (args.join(' ').toLocaleLowerCase() == 'items') {
+            let embed1 = new MessageEmbed()
+                .setColor("GREEN")
+                .setDescription(`❌ You don't have Nikes to sell`);
+
+            let items = await client.db.fetch(`nikes_${user.id}`)
+
+            if (items < 1) return message.channel.send(embed1)
+
+            client.db.fetch(`nikes_${user.id}`)
+            client.db.subtract(`nikes_${user.id}`, 1)
+        }
+}
