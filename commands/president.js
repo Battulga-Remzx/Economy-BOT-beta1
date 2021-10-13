9caaabf171a260623770cc7cdb9e81eca7bd8bb1
Glitch (remzx-official-economy-bot-active) <none>
2021-10-13 16:31:48 +0000

    🍭👠 Checkpoint ./commands/beg.js:36148879/28 ./commands/president.js:36148879/1563

--- commands/president.js
+++ commands/president.js
@@ -1,12 +1,19 @@
 exports.execute = async (client, message, args) => {
-    let amount = Math.floor(Math.random() * 30000-29999) + 1;
-    let pre = client.eco.pre(client.ecoAddUser, amount);
-    if (pre.onCooldown) return message.reply(`Чи аль хэдийнээ авчихсан байна дараа дахин хийнэ үү! ${pre.time.hours} hours, ${pre.time.minutes} minutes & ${pre.time.seconds} seconds to claim it again.`);
-    else return message.reply(`Мөнгө амжилттай авагдлаа **${pre.amount}** 💸чамд одоо байгаа **${pre.after}** 💸байна.`);
+  if(client.config.president.includes(message.author.id)) return;
+  let amount = Math.floor(Math.random() * 30000) + 20000;
+  let pre = client.eco.beg(client.db.add(`amount_$`), amount);
+  if (pre.onCooldown)
+    return message.reply(
+      `хугацаа болоогүй байна ${pre.time.seconds} секундын дараа`
+    );
+  else
+    return message.reply(
+      `Мөнгө амжилттай авагдлаа **${pre.amount}** 💸чамд одоо байгаа **${pre.after}** 💸байна.`
+    );
 };
 
-module.exports.help = {
-    name: "---------------Өдөр тутмын орлого----------",
-    aliases: ["pre"],
-    usage: "pre"
-}
+exports.help = {
+  name: "---------------Өдөр тутмын орлого----------",
+  aliases: ["pre"],
+  usage: "pre"
+};
