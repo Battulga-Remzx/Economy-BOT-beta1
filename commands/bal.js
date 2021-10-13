9473ef89fcf9a9e9636718456f1f648d39bbcc42
Glitch (remzx-official-economy-bot-active) <none>
2021-10-13 18:18:48 +0000

    👯👐 Checkpoint ./commands/bal.js:36148879/322

--- commands/bal.js
+++ commands/bal.js
@@ -1,20 +1,22 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
+  let bank = await client.db.fetch(`bank_${message.user.id}`);
   let user = message.mentions.users.first() || message.author;
   let userBalance = client.eco.fetchMoney(user.id);
+  
   const embed = new MessageEmbed()
     .setTitle(`Данс шалгаж байна`)
     .addField(`Хэрэглэгчийн нэр`, `<@${userBalance.user}>`)
     .addField(`Мөнгө`, `${userBalance.amount} 💸байна`)
+  .addField(`Данс`, `${bank.amount}💸байна`)
     .addField(`Leaderboard Rank`, `Top ${userBalance.position} -т жигсаж байна`)
     .setColor("RANDOM")
     .setThumbnail(user.displayAvatarURL)
     .setTimestamp();
   return message.channel.send(embed);
 
-  let bank = client.db.fetch(`bank_${message.guild.id}_${user.id}`);
-  if (bank === null) bank = 0;
+  
 };
 exports.help = {
   name: "---------------Дансаа шалгах----------",
