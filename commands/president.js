6247ef15bb45751d026501bf97935a8edbaaa88d
Glitch (remzx-official-economy-bot-active) <none>
2021-10-13 15:57:25 +0000

    🐛🐟 Checkpoint ./commands/president.js:36148879/1045

--- commands/president.js
+++ commands/president.js
@@ -1,7 +1,17 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
-let author = message.author
-let amount = Math.floor(Math.random() * 10000-9999) + 1;
-  let pre = client.eco.pre(client.ecoAddUser, amoint)
-}
+  if(client.config.president.includes(message.))
+  let author = message.author;
+  let amount = Math.floor(Math.random() * 10000 - 9999) + 1;
+  let pre = client.eco.pre(client.ecoAddUser, amount);
+if (pre.onCooldonwns) return message.reply(`арай болоогүй байна ${pre.time.minutes} минут ${pre.time.seconds}секунд`)
+  else return message.reply(`мөнгө амжилттай орлоо ${pre.amount}`)
+};
+
+exports.help = {
+  name: "---------------Ерөнхийлөгч----------",
+  aliases: ["pre"],
+  usage: `pre`
+};
+
