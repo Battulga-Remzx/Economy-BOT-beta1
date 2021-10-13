59e5f68ed5dc603f914666f2c3e59f523c2210f4
Glitch (remzx-official-economy-bot-active) <none>
2021-10-13 16:08:44 +0000

    🚛🏛 Checkpoint ./commands/president.js:36148879/17

--- commands/president.js
+++ commands/president.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
-  if(client.config.president.includes(message.))
+  if(client.config.president.includes(message.author.id)) return;
   let author = message.author;
   let amount = Math.floor(Math.random() * 10000 - 9999) + 1;
   let pre = client.eco.pre(client.ecoAddUser, amount);
