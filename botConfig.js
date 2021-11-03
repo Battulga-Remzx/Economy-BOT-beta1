f84f7d230d74d75ddc8101bb04beb2a89bc1f11e
Battulga-Remzx <89357725+Battulga-Remzx@users.noreply.github.com>
2021-11-03 19:04:37 +0800

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,13 +1,13 @@
 module.exports = {
-  token: "ODg3Mzk4MDExOTAxMTk0MjQw.YUDjzQ.ROF8hs4iRt-M9d4MDym29EpZ9qg",
+  token: "Bot Token-өө оруулана уу",
   prefix: "~",
-  admins: ["715428540954181714"],
-  mafias: ["Jija","811447438472380459","Azaa","877484899517468712",""],
+  admins: ["Админ ID оруул"],
+  mafias: [""],
   miners: [""],
-  polices: ["Nymhuu","886946732783570986","Batuka","715428540954181714","Puba","847435035842052146"],
+  polices: [""],
   debug: true,
-  president: ["765593479010320424",],
-  test: ["715428540954181714"],
+  president: ["",],
+  test: [""],
 
   countChannel: "countChannelID"
 };
